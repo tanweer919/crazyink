a8f7c3eaa56aa1318bb462b0b370d50281f82f76
tanweer anwar <tanweer.anwar919@gmail.com>
2017-12-30 15:27:44 +0530

    Add basic backend logic

--- app.js
+++ app.js
@@ -1,46 +1,47 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const http = require('http');
+const cookieParser = require("cookie-parser");
+const validator = require("express-validator");
+var session = require("express-session");
+const flash = require("express-flash");
+const MongoStore = require("connect-mongo")(session);
+const mongoose = require("mongoose");
+const passport = require("passport");
+const route = require("./controllers/users");
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/crazyink", {useMongoClient: true});
+const app = setupExpress();
 
-var index = require('./routes/index');
-var users = require('./routes/users');
 
-var app = express();
+function setupExpress() {
+    const app = express();
+    const server = http.createServer(app);
+    server.listen(3000, function () {
+        console.log("Listenig on port 3000")
+    });
+    configureExpress(app);
+}
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+function configureExpress(app) {
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'ejs');
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(cookieParser());
+    app.use(session({
+        secret: "Welcome to hell",
+        resave: false,
+        saveUninitialized: false,
+        store: new MongoStore({mongooseConnection: mongoose.connection})
+    }))
+    app.use(flash());
+    app.use(validator());
+    app.use(passport.initialize());
+    app.use(passport.session());
+    app.use(route);
+}
 
-app.use('/', index);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
